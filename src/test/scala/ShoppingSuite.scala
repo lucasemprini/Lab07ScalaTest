5952efd01bb243b623a4e0eab87f86d9df312595
luca.semprini10 <luca.semprini10@studio.unibo.it>
2018-04-19 11:19:17 +0200

    Cercando di capire cosa fare

---
+++ src/test/scala/ShoppingSuite.scala
@@ -0,0 +1,16 @@
+import javax.swing.plaf.basic.BasicTextUI.BasicCaret
+
+import org.scalatest.FunSuite
+import u07lab.Shopping
+
+
+class CartSuite extends FunSuite {
+}
+
+class CatalogSuite extends FunSuite {
+
+}
+
+class WarehouseSuite extends FunSuite {
+
+}
