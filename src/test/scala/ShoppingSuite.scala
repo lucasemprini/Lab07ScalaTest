ec3e2f303d9163a757d1603d69fedfcefff4a070
luca.semprini10 <luca.semprini10@studio.unibo.it>
2018-04-19 11:49:22 +0200

    Giornata del cazzo

--- src/test/scala/ShoppingSuite.scala
+++ src/test/scala/ShoppingSuite.scala
@@ -1,14 +1,25 @@
-import javax.swing.plaf.basic.BasicTextUI.BasicCaret
-
 import org.scalatest.FunSuite
-import u07lab.Shopping
+import u07lab._
+
+import scala.collection.immutable.HashMap
 
 
 class CartSuite extends FunSuite {
+  test("Cart should add items") {
+    val cart: Cart = new BasicCart(Map.empty)
+    val foc: Item = Item(Product("Focaccine Esselunga"), ItemDetails(1, Price(2000)))
+    cart.add(foc)
+    assert(cart.size > 0)
+    assert(cart.size == 1)
+    assert(cart.totalCost == 2000)
+  }
 }
 
 class CatalogSuite extends FunSuite {
-
+  test("Cart should add items") {
+    val foc: Product = Product("Focaccine Esselunga")
+    val cat: Catalog = new BasicCatalog(new HashMap[Product, Price]())
+  }
 }
 
 class WarehouseSuite extends FunSuite {
